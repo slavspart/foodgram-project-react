0e817e152af564f50d30f39fa2325409b7f23080
Святослав М <slavspart@yandex.ru>
2022-11-11 23:04:07 +0300

    deploy13

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('DJANGO_KEY',)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('HOSTS'), '127.0.0.1']
+ALLOWED_HOSTS = [os.getenv('HOSTS'), '127.0.0.1', 'backend']
 
 
 # Application definition
